44751f18baf74eebe51695c4ddbedb494d3bbbf3
Lucyna Sztemberg <lucynasztemberg@o2.pl>
2018-01-12 14:41:49 +0100

    Add BookController

---
+++ src/main/java/pl/coderslab/apiback/controller/BookController.java
@@ -0,0 +1,21 @@
+package pl.coderslab.apiback.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pl.coderslab.apiback.model.Book;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+	
+	@GetMapping("/test")
+	public Book test() {
+		//zwroci w przegladarce w formacie JSON
+		return new Book (1L,"9788324631766","Thinking	in	Java",
+				"Bruce	Eckel","Helion","programming");
+	}
+	
+	
+}
